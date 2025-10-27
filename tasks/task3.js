632ebfb8cb138754b64ec0b0d25577f42a2f19bc
kn24-mrytchenko-prog <kn24-m.rytchenko@nubip.edu.ua>
2025-10-27 16:16:43 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,15 +1,11 @@
-// Маємо об'єкт з інформацією про види спорту, кількість представників і кількість медалей. 
-// Необхідно додати нове поле percentOfSuccess, яке буде показувати відсоток завойованих медалей від загальної кількості спортсменів у кожному виді спорту.
-"ВИКОРИСТОВУВАТИ ЛИШЕ МЕТОДИ МАСИВІВ filter, map, sort та інші, які є в файлі methods.js."
-"Можливо в цій задачі доведеться в методі map розкривати фігурні дужки для роботи з обʼєктом."
-// Щось тиру Array.map(item => {
-//   blablabla
-//  .. return ЩОСЬ)
-// })
-"Для заокруглення числа можна до десятих використовуйте .toFixed(1)"
-
 function addSuccessPercent(olympicRepresentation) {
-  // Ваш код
+  return olympicRepresentation.map(item => {
+    const percent = (item.medals / item.athletes) * 100;
+    return {
+      ...item,
+      percentOfSuccess: percent.toFixed(1)
+    };
+  });
 }
 
 const olympicRepresentation = [
