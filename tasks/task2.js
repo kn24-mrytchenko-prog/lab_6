4d995b75f91842af43af958942883cbc033cfaeb
kn24-mrytchenko-prog <kn24-m.rytchenko@nubip.edu.ua>
2025-10-27 16:13:49 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,6 +1,8 @@
-// Функція отримує масив товарів і повертає всі товари, які мають ціну нижчу за 100 або вищу за 500
-"ВИКОРИСТОВУВАТИ ЛИШЕ МЕТОДИ МАСИВІВ filter, map, sort та інші, які є в файлі methods.js."
+function getProductsNotInPriceRange(products) {
+  return products.filter(product => product.price < 100 || product.price > 500);
+}
 
+// Приклад використання:
 const products = [
   { name: 'Laptop', price: 800 },
   { name: 'Phone', price: 250 },
@@ -11,9 +13,14 @@ const products = [
   { name: 'PlayStation', price: 590 }
 ];
 
-function getProductsNotInPriceRange(products) {
-  // Ваш код
-}
-
 console.log(getProductsNotInPriceRange(products));
+// Виведе:
+// [
+//   { name: 'Laptop', price: 800 },
+//   { name: 'Shirt', price: 30 },
+//   { name: 'Tablet', price: 550 },
+//   { name: 'Shoes', price: 75 },
+//   { name: 'PlayStation', price: 590 }
+// ]
+
 module.exports = getProductsNotInPriceRange;
