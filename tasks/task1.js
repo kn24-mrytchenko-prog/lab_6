9495605804c28d7d9b637fd35f41f4a190c5c22d
kn24-mrytchenko-prog <kn24-m.rytchenko@nubip.edu.ua>
2025-10-27 16:10:26 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,10 +1,13 @@
-// Задача 1: Написати функцію, яка повертає найпопулярніші товарні категорії на основі продажів.
-// Спочатку фільтруємо товари, які продані більше 5 разів, потім сортуємо їх за кількістю продажів,
-// а потім залишаємо тільки унікальні категорії товарів.
-"ВИКОРИСТОВУВАТИ ЛИШЕ МЕТОДИ МАСИВІВ filter, map, sort. Для того щоб залишити лише унікальні категорії товарів можна використати конструкцію new Set, або використати метод filter()/indexOf()/forEach()"
-
 function getPopularCategories(products) {
-  //Ваш код
+  const popularProducts = products.filter(product => product.sales > 5);
+
+  const sortedProducts = popularProducts.sort((a, b) => b.sales - a.sales);
+
+  const categories = sortedProducts.map(product => product.category);
+
+  const uniqueCategories = categories.filter((category, index) => categories.indexOf(category) === index);
+
+  return uniqueCategories;
 }
 
 // Приклад використання:
@@ -19,4 +22,5 @@ const products = [
 ];
 
 console.log(getPopularCategories(products)); // ['Electronics', 'Clothing', 'Footwear']
+
 module.exports = getPopularCategories;
